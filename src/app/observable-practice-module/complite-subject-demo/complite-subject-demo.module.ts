d46811b2a30f865d324b481d01d094b751a569ba
jaydipgohilpel <jaydip.gohil@proexelancers.com>
2022-11-16 15:18:12 +0530

    code refactor

--- src/app/observable-practice-module/complite-subject-demo/complite-subject-demo.module.ts
+++ src/app/observable-practice-module/complite-subject-demo/complite-subject-demo.module.ts
@@ -1,11 +1,9 @@
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CompliteLeftbarComponent } from './complite-leftbar/complite-leftbar.component';
 import { CompliteFormCompoComponent } from './complite-form-compo/complite-form-compo.component';
 import { CompliteDatatableComponent } from './complite-datatable/complite-datatable.component';
 import { CompliteDashboardComponent } from './complite-dashboard/complite-dashboard.component';
-import { TableModule } from 'primeng/table';
 import { CompliteFooterComponent } from './complite-footer/complite-footer.component';
 import { NavbarModule } from 'src/app/navbar/navbar.module';
 import { SharedModule } from 'src/app/shared/shared.module';
--- src/app/observable-practice-module/observable-practice-module.module.ts
+++ src/app/observable-practice-module/observable-practice-module.module.ts
@@ -1,5 +1,4 @@
 import { NavbarModule } from './../navbar/navbar.module';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FirstComponuntComponent } from './first-componunt/first-componunt.component';
@@ -7,7 +6,6 @@ import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
 import { SecondComponuntComponent } from './second-componunt/second-componunt.component';
 import { ThirdComponuntComponent } from './third-componunt/third-componunt.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { InputSwitchModule } from 'primeng/inputswitch';
 import { CompliteSubjectDemoModule } from './complite-subject-demo/complite-subject-demo.module';
 import { SharedModule } from '../shared/shared.module';
 
