91b9f046ecd3991e66e41269224f55531817175f
jaydipgohilpel <jaydip.gohil@proexelancers.com>
2022-11-18 14:56:12 +0530

    angular 14...

--- src/app/angular-14-practices/main-component/main-component.component.ts
+++ src/app/angular-14-practices/main-component/main-component.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-component',
   templateUrl: './main-component.component.html',
-  styleUrls: ['./main-component.component.scss']
+  styleUrls: ['./main-component.component.scss'],
 })
 export class MainComponentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  @HostListener('click', ['$event'])
+  showOnClick(event: any) {
+    alert('@HostListener');
   }
-
 }
